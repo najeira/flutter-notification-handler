bd9f6114144c0316f38d65bd3e7756502ef782f4
najeira <najeira@gmail.com>
2020-02-19 11:33:03 +0900

    first implementation

--- lib/notification_handler.dart
+++ lib/notification_handler.dart
@@ -1,7 +1,3 @@
 library notification_handler;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/notification_handler.dart';
